eebd091011e4c779b62f5b85e75fbca74c88334f
Kristin M Oberlander <koberlander115@gmail.com>
2018-11-04 15:12:02 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,10 +6,14 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index() 
 
+def input_to_index() 
+ask for number [1,2,3,4,5,6,7,8,9]
+convert to array index 
 end
 
+
 def move
-  
+fill position with X or O 
+puts updated board 
 end
